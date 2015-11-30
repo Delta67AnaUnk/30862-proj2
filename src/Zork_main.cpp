4cf51b43002dd85668fc398b6f7356dcb61ddb1a
Tian Qiu <qiu58@purdue.edu>
2015-11-30 01:21:52 -0500

    xml loading Main

--- src/Zork_main.cpp
+++ src/Zork_main.cpp
@@ -1,9 +1,41 @@
 #include "Zork_main.hpp"
-#include "rapidxml/rapidxml.hpp"
+#include <fstream>
 
 
-Map loadMap(string& filename)
-{
+Game::Game(string filename) {
+    try {
+        gameReady = loadXML(filename);
+    } catch (int error) {
+        cout << "Error parsing the XML." << endl;
+    }
+}
+
+Game::~Game() {
 
 }
+
+bool Game::loadXML(string filename) {
+    xml_document<> doc;
+    xml_node<> * root_node;
+    // Read the xml file into a vector
+    ifstream f;
+    f.open(filename);
+    if (f.fail()) {
+        perror("sdfsdfasfsdfas");
+        cout << "Opening file error, exit!!! " << endl;
+        return false;
+    }
+    vector<char> buffer((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
+    buffer.push_back('\0');
+    
+    return true;
+}
+
+
+
+void Game::startGame() {
+    //Always start at entrance
+    currentRoom = "Entrance";
+}
+
 
--- src/Zork_main.hpp
+++ src/Zork_main.hpp
@@ -1,22 +1,48 @@
 #ifndef _ZORK_MAIN_
 #define _ZORK_MAIN_
 
+
 #include <list>
-#include "Object.hpp"
+/*#include "Object.hpp"
 #include "Zork_Def.hpp"
 #include "Room.hpp"
 #include <functional>
 #include "Map.hpp"
 #include "Container"
+ */
+#include <string>
+#include <stdio.h>
+#include <iostream>
+
+#include <iterator>
+#include <vector>
+#include <map>
+#include <queue>
+#include "rapidxml/rapidxml.hpp"
 
 using namespace std;
+using namespace rapidxml;
 
+
+/*
 Map ZorkMap;
-
 Container Inventory;
 Room* CurRoomPtr;
-
 Map loadMap(string& filename);
+*/
+class Game {
+public:
+    
+    Game(string);
+    virtual ~Game();
+    void startGame();
+    bool gameReady = false;
+    string currentRoom;
+    
+private:
+    bool loadXML(string filename);
+    
+};
 
 
 #endif
